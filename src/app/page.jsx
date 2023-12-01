92806969b5bbf722f8d7e9f8618153070cedd7e6
Ricardo Haro <99053383+Skianyan@users.noreply.github.com>
2023-12-01 14:44:38 -0800

    push

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,12 @@
+import Link from "next/link";
 import React from "react";
 
 const Main = () => {
-	return <div>Main</div>;
+	return (
+		<div>
+			<Link href={"/api/auth/signin"}>Sign In</Link>
+		</div>
+	);
 };
 
 export default Main;
