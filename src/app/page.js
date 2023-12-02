50880ee18cae06e06844e637ec269af9a3b0c6c0
Ricardo Haro <99053383+Skianyan@users.noreply.github.com>
2023-12-01 16:00:46 -0800

    firestore connected

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,18 @@
 import Link from "next/link";
 import React from "react";
+import { addDoc, collection } from "firebase/firestore";
+import { FIRESTORE_DB } from "../utils/firebase";
 
-const Main = () => {
+const Main = async () => {
+	const addUser = async () => {
+		const user = await addDoc(collection(FIRESTORE_DB, "testcoll"), {
+			name: "testname",
+		});
+	};
 	return (
 		<div>
 			<Link href={"/api/auth/signin"}>Sign In</Link>
+			<button onClick={addUser()}>add test</button>
 		</div>
 	);
 };
