36d012913884866af76b66926b86c8affc5f693d
Ricardo Haro <99053383+Skianyan@users.noreply.github.com>
2023-12-05 15:08:07 -0800

    Update PostsCard.jsx

--- src/app/components/PostsCard.jsx
+++ src/app/components/PostsCard.jsx
@@ -9,10 +9,13 @@ const PostsCard = ({ posts }) => {
 	return (
 		<div>
 			{posts.length > 0 ? (
-				<div className="lg:w-full w-[95vw]">
+				<div className="lg:w-full w-[95vw] flex flex-col justify-center items-center">
 					{posts.map((post) => {
 						return (
-							<div key={post.id} className="bg-[#285C4D] p-5 rounded-lg m-4">
+							<div
+								key={post.id}
+								className="bg-[#285C4D] p-5 rounded-lg m-4 w-[40vw]"
+							>
 								<div>Title: {post.title}</div>
 								<div>Body: {post.body}</div>
 								<div>Date: {post.date}</div>
